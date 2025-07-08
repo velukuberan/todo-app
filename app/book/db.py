dd6cd4ab052974edee83bed799e6e785c3710aa8
Velmurugan K <97747445+velukuberan@users.noreply.github.com>
2025-07-08 12:50:17 +0530

    feat: add basic routes to user, book and shipping (#8)
    * feat: add basic routes to user, book and shipping
    
    TAUF-2

---
+++ app/book/db.py
@@ -0,0 +1,402 @@
+books = {
+    1: {
+        "isbn": "978-0-553-29698-2",
+        "title": "A Game of Thrones",
+        "genre": "Fantasy, Adventure, Political Drama",
+        "author": "George R.R. Martin",
+        "price": 14.99,
+        "release_year": 1996
+    },
+    2: {
+        "isbn": "978-0-7432-7356-5",
+        "title": "The Stand",
+        "genre": "Horror, Post-Apocalyptic, Science Fiction",
+        "author": "Stephen King",
+        "price": 18.99,
+        "release_year": 1978
+    },
+    3: {
+        "isbn": "978-0-06-112008-4",
+        "title": "To Kill a Mockingbird",
+        "genre": "Fiction, Drama, Coming-of-Age",
+        "author": "Harper Lee",
+        "price": 12.99,
+        "release_year": 1960
+    },
+    4: {
+        "isbn": "978-0-547-92822-7",
+        "title": "The Hobbit",
+        "genre": "Fantasy, Adventure, Children's Literature",
+        "author": "J.R.R. Tolkien",
+        "price": 13.99,
+        "release_year": 1937
+    },
+    5: {
+        "isbn": "978-0-14-243724-7",
+        "title": "1984",
+        "genre": "Dystopian, Political Fiction, Science Fiction",
+        "author": "George Orwell",
+        "price": 11.99,
+        "release_year": 1949
+    },
+    6: {
+        "isbn": "978-0-7653-0633-0",
+        "title": "The Way of Kings",
+        "genre": "Epic Fantasy, High Fantasy",
+        "author": "Brandon Sanderson",
+        "price": 19.99,
+        "release_year": 2010
+    },
+    7: {
+        "isbn": "978-0-316-76948-0",
+        "title": "The Catcher in the Rye",
+        "genre": "Fiction, Coming-of-Age, Literary Fiction",
+        "author": "J.D. Salinger",
+        "price": 10.99,
+        "release_year": 1951
+    },
+    8: {
+        "isbn": "978-0-525-47535-5",
+        "title": "Where the Crawdads Sing",
+        "genre": "Mystery, Literary Fiction, Nature",
+        "author": "Delia Owens",
+        "price": 15.99,
+        "release_year": 2018
+    },
+    9: {
+        "isbn": "978-0-439-70818-8",
+        "title": "Harry Potter and the Sorcerer's Stone",
+        "genre": "Fantasy, Young Adult, Adventure",
+        "author": "J.K. Rowling",
+        "price": 12.99,
+        "release_year": 1997
+    },
+    10: {
+        "isbn": "978-0-06-440055-8",
+        "title": "Where the Red Fern Grows",
+        "genre": "Children's Literature, Adventure, Nature",
+        "author": "Wilson Rawls",
+        "price": 8.99,
+        "release_year": 1961
+    },
+    11: {
+        "isbn": "978-0-385-33312-0",
+        "title": "The Handmaid's Tale",
+        "genre": "Dystopian, Feminist Fiction, Science Fiction",
+        "author": "Margaret Atwood",
+        "price": 14.99,
+        "release_year": 1985
+    },
+    12: {
+        "isbn": "978-0-307-58837-1",
+        "title": "Gone Girl",
+        "genre": "Psychological Thriller, Mystery, Crime",
+        "author": "Gillian Flynn",
+        "price": 16.99,
+        "release_year": 2012
+    },
+    13: {
+        "isbn": "978-0-06-085052-4",
+        "title": "The Kite Runner",
+        "genre": "Historical Fiction, Drama, War",
+        "author": "Khaled Hosseini",
+        "price": 13.99,
+        "release_year": 2003
+    },
+    14: {
+        "isbn": "978-0-345-34296-8",
+        "title": "Dune",
+        "genre": "Science Fiction, Space Opera, Political Fiction",
+        "author": "Frank Herbert",
+        "price": 17.99,
+        "release_year": 1965
+    },
+    15: {
+        "isbn": "978-0-553-21311-4",
+        "title": "Fahrenheit 451",
+        "genre": "Dystopian, Science Fiction, Political Fiction",
+        "author": "Ray Bradbury",
+        "price": 11.99,
+        "release_year": 1953
+    },
+    16: {
+        "isbn": "978-0-7434-7679-3",
+        "title": "The Great Gatsby",
+        "genre": "Literary Fiction, Tragedy, Jazz Age",
+        "author": "F. Scott Fitzgerald",
+        "price": 9.99,
+        "release_year": 1925
+    },
+    17: {
+        "isbn": "978-0-553-80716-0",
+        "title": "A Clash of Kings",
+        "genre": "Fantasy, Adventure, Political Drama",
+        "author": "George R.R. Martin",
+        "price": 15.99,
+        "release_year": 1998
+    },
+    18: {
+        "isbn": "978-0-841-2731-2",
+        "title": "Life of Pi",
+        "genre": "Adventure, Philosophical Fiction, Survival",
+        "author": "Yann Martel",
+        "price": 13.99,
+        "release_year": 2001
+    },
+    19: {
+        "isbn": "978-0-7432-4722-1",
+        "title": "The Da Vinci Code",
+        "genre": "Mystery, Thriller, Historical Fiction",
+        "author": "Dan Brown",
+        "price": 14.99,
+        "release_year": 2003
+    },
+    20: {
+        "isbn": "978-0-525-55441-1",
+        "title": "Educated",
+        "genre": "Memoir, Biography, Non-Fiction",
+        "author": "Tara Westover",
+        "price": 16.99,
+        "release_year": 2018
+    },
+    21: {
+        "isbn": "978-0-06-112241-5",
+        "title": "The Alchemist",
+        "genre": "Philosophical Fiction, Adventure, Spiritual",
+        "author": "Paulo Coelho",
+        "price": 12.99,
+        "release_year": 1988
+    },
+    22: {
+        "isbn": "978-0-375-50420-7",
+        "title": "The Road",
+        "genre": "Post-Apocalyptic, Literary Fiction, Survival",
+        "author": "Cormac McCarthy",
+        "price": 14.99,
+        "release_year": 2006
+    },
+    23: {
+        "isbn": "978-0-452-28423-4",
+        "title": "Brave New World",
+        "genre": "Dystopian, Science Fiction, Social Commentary",
+        "author": "Aldous Huxley",
+        "price": 13.99,
+        "release_year": 1932
+    },
+    24: {
+        "isbn": "978-0-316-01218-3",
+        "title": "The Shining",
+        "genre": "Horror, Psychological Thriller, Supernatural",
+        "author": "Stephen King",
+        "price": 15.99,
+        "release_year": 1977
+    },
+    25: {
+        "isbn": "978-0-439-13959-5",
+        "title": "The Hunger Games",
+        "genre": "Dystopian, Young Adult, Science Fiction",
+        "author": "Suzanne Collins",
+        "price": 12.99,
+        "release_year": 2008
+    },
+    26: {
+        "isbn": "978-0-06-085566-6",
+        "title": "A Thousand Splendid Suns",
+        "genre": "Historical Fiction, Drama, War",
+        "author": "Khaled Hosseini",
+        "price": 14.99,
+        "release_year": 2007
+    },
+    27: {
+        "isbn": "978-0-14-017739-8",
+        "title": "One Hundred Years of Solitude",
+        "genre": "Magical Realism, Literary Fiction, Family Saga",
+        "author": "Gabriel García Márquez",
+        "price": 16.99,
+        "release_year": 1967
+    },
+    28: {
+        "isbn": "978-0-375-41398-8",
+        "title": "The Book Thief",
+        "genre": "Historical Fiction, War, Young Adult",
+        "author": "Markus Zusak",
+        "price": 13.99,
+        "release_year": 2005
+    },
+    29: {
+        "isbn": "978-0-7432-7356-6",
+        "title": "It",
+        "genre": "Horror, Supernatural, Coming-of-Age",
+        "author": "Stephen King",
+        "price": 19.99,
+        "release_year": 1986
+    },
+    30: {
+        "isbn": "978-0-06-440020-6",
+        "title": "Of Mice and Men",
+        "genre": "Literary Fiction, Drama, Tragedy",
+        "author": "John Steinbeck",
+        "price": 10.99,
+        "release_year": 1937
+    },
+    31: {
+        "isbn": "978-0-7653-4677-1",
+        "title": "Words of Radiance",
+        "genre": "Epic Fantasy, High Fantasy",
+        "author": "Brandon Sanderson",
+        "price": 20.99,
+        "release_year": 2014
+    },
+    32: {
+        "isbn": "978-0-307-38870-8",
+        "title": "The Girl with the Dragon Tattoo",
+        "genre": "Crime, Mystery, Thriller",
+        "author": "Stieg Larsson",
+        "price": 15.99,
+        "release_year": 2005
+    },
+    33: {
+        "isbn": "978-0-345-80371-6",
+        "title": "Ready Player One",
+        "genre": "Science Fiction, Dystopian, Adventure",
+        "author": "Ernest Cline",
+        "price": 14.99,
+        "release_year": 2011
+    },
+    34: {
+        "isbn": "978-0-7432-4722-2",
+        "title": "Angels & Demons",
+        "genre": "Mystery, Thriller, Religious Fiction",
+        "author": "Dan Brown",
+        "price": 13.99,
+        "release_year": 2000
+    },
+    35: {
+        "isbn": "978-0-553-38268-4",
+        "title": "A Storm of Swords",
+        "genre": "Fantasy, Adventure, Political Drama",
+        "author": "George R.R. Martin",
+        "price": 16.99,
+        "release_year": 2000
+    },
+    36: {
+        "isbn": "978-0-7432-7357-3",
+        "title": "The Green Mile",
+        "genre": "Fantasy, Drama, Crime",
+        "author": "Stephen King",
+        "price": 15.99,
+        "release_year": 1996
+    },
+    37: {
+        "isbn": "978-0-06-112009-1",
+        "title": "Go Set a Watchman",
+        "genre": "Fiction, Drama, Literary Fiction",
+        "author": "Harper Lee",
+        "price": 13.99,
+        "release_year": 2015
+    },
+    38: {
+        "isbn": "978-0-385-49081-8",
+        "title": "The Fault in Our Stars",
+        "genre": "Young Adult, Romance, Drama",
+        "author": "John Green",
+        "price": 11.99,
+        "release_year": 2012
+    },
+    39: {
+        "isbn": "978-0-06-073132-6",
+        "title": "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+        "genre": "Fantasy, Children's Literature, Adventure",
+        "author": "C.S. Lewis",
+        "price": 9.99,
+        "release_year": 1950
+    },
+    40: {
+        "isbn": "978-0-307-47428-9",
+        "title": "The Help",
+        "genre": "Historical Fiction, Drama, Social Issues",
+        "author": "Kathryn Stockett",
+        "price": 14.99,
+        "release_year": 2009
+    },
+    41: {
+        "isbn": "978-0-7653-6304-4",
+        "title": "Oathbringer",
+        "genre": "Epic Fantasy, High Fantasy",
+        "author": "Brandon Sanderson",
+        "price": 21.99,
+        "release_year": 2017
+    },
+    42: {
+        "isbn": "978-0-399-59135-1",
+        "title": "The Seven Husbands of Evelyn Hugo",
+        "genre": "Historical Fiction, Romance, LGBTQ+",
+        "author": "Taylor Jenkins Reid",
+        "price": 15.99,
+        "release_year": 2017
+    },
+    43: {
+        "isbn": "978-0-7432-7358-0",
+        "title": "Carrie",
+        "genre": "Horror, Supernatural, Young Adult",
+        "author": "Stephen King",
+        "price": 13.99,
+        "release_year": 1974
+    },
+    44: {
+        "isbn": "978-0-345-80398-3",
+        "title": "Armada",
+        "genre": "Science Fiction, Young Adult, Adventure",
+        "author": "Ernest Cline",
+        "price": 13.99,
+        "release_year": 2015
+    },
+    45: {
+        "isbn": "978-0-553-57340-1",
+        "title": "A Feast for Crows",
+        "genre": "Fantasy, Adventure, Political Drama",
+        "author": "George R.R. Martin",
+        "price": 16.99,
+        "release_year": 2005
+    },
+    46: {
+        "isbn": "978-0-375-81307-8",
+        "title": "Cloud Atlas",
+        "genre": "Science Fiction, Literary Fiction, Historical Fiction",
+        "author": "David Mitchell",
+        "price": 17.99,
+        "release_year": 2004
+    },
+    47: {
+        "isbn": "978-0-06-231500-7",
+        "title": "The Immortalists",
+        "genre": "Literary Fiction, Family Saga, Magical Realism",
+        "author": "Chloe Benjamin",
+        "price": 15.99,
+        "release_year": 2018
+    },
+    48: {
+        "isbn": "978-0-7653-9073-6",
+        "title": "Rhythm of War",
+        "genre": "Epic Fantasy, High Fantasy",
+        "author": "Brandon Sanderson",
+        "price": 22.99,
+        "release_year": 2020
+    },
+    49: {
+        "isbn": "978-0-553-41811-6",
+        "title": "A Dance with Dragons",
+        "genre": "Fantasy, Adventure, Political Drama",
+        "author": "George R.R. Martin",
+        "price": 17.99,
+        "release_year": 2011
+    },
+    50: {
+        "isbn": "978-0-525-51032-9",
+        "title": "Becoming",
+        "genre": "Memoir, Biography, Non-Fiction",
+        "author": "Michelle Obama",
+        "price": 18.99,
+        "release_year": 2018
+    }
+}
---
+++ app/book/route.py
@@ -0,0 +1,30 @@
+from typing import List
+from fastapi import APIRouter
+from .schema import Book 
+from .db import books
+
+router = APIRouter(prefix="/books", tags=["books"])
+
+@router.get("/", response_model=List[Book])
+def get_books():
+    return [
+        Book(id=book_id, **data) for book_id, data in books.items()
+    ]
+
+@router.get("/{id}", response_model=Book)
+def get_books(id: int):
+
+    if id not in books:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Book #id:{id} doesn't exists"
+        )
+
+    book = Book(
+        id=id,
+        **books[id]
+    )
+    
+    book.add_genre('Thriller')
+
+    return book
