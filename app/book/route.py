c31614f35dec5a3752dfb0062514134c467fe726
Velmurugan Kuberan <v.kuberan@syde.com>
2025-07-10 19:15:21 +0530

    fix: add scheme to shipments
    TAUF-4

--- app/book/route.py
+++ app/book/route.py
@@ -1,18 +1,18 @@
 from typing import List
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from .schema import Book 
 from .db import books
 
 router = APIRouter(prefix="/books", tags=["books"])
 
 @router.get("/", response_model=List[Book])
-def get_books():
+def get_all_books():
     return [
         Book(id=book_id, **data) for book_id, data in books.items()
     ]
 
 @router.get("/{id}", response_model=Book)
-def get_books(id: int):
+def get_book_by_id(id: int):
 
     if id not in books:
         raise HTTPException(
