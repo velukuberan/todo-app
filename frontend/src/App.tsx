41e124c42e8a3679ec802d59f36a23ba4738a483
udhayakumar <udhaya0000@gmail.com>
2025-07-12 22:35:10 +0530

    ui components added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
 import './App.css';
 import { Button } from '@ui/button';
 import { Checkbox } from '@ui/checkbox';
+import { Input } from '@ui/input';
+import { RadioGroup, RadioGroupItem } from '@ui/radio-group';
 
 function App() {
 
@@ -11,7 +12,45 @@ function App() {
         <Button variant="primary">Primary Button</Button>
       </div>
       <div className='mt-4'>
-        <Checkbox>Secondary Checkbox</Checkbox>
+        <label className="flex items-center gap-2">
+          <Checkbox />
+          <span>
+            Accept terms and conditions.
+          </span>
+        </label>
+      </div>
+      <div className='mt-4'>
+        <Input
+          type="text"
+          placeholder="Enter your name"
+          className="mt-4"
+        />
+      </div>
+      <div className='mt-4'>
+        <RadioGroup
+          className="RadioGroupRoot"
+          defaultValue="default"
+          aria-label="View density"
+        >
+          <div className='flex items-center gap-1'>
+            <RadioGroupItem className="RadioGroupItem" value="default" id="r1" />
+            <label className="Label" htmlFor="r1">
+              Default
+            </label>
+          </div>
+          <div className='flex items-center gap-1'>
+            <RadioGroupItem className="RadioGroupItem" value="comfortable" id="r2" />
+            <label className="Label" htmlFor="r2">
+              Comfortable
+            </label>
+          </div>
+          <div className='flex items-center gap-1'>
+            <RadioGroupItem className="RadioGroupItem" value="compact" id="r3" />
+            <label className="Label" htmlFor="r3">
+              Compact
+            </label>
+          </div>
+        </RadioGroup>
       </div>
     </div>
   )
